df0692686d7df2af6a86d792346a09c2e37b5c78
cedric karungu <ckarungu921@gmail.com>
2022-10-05 10:15:24 +0200

    feat: add useSelector to get state from redux

--- src/components/Phone.js
+++ src/components/Phone.js
@@ -1,32 +1,35 @@
 import React from 'react';
 import phone from '../assets/images/phone.jpg';
-import { connect } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { buyPhone } from '../redux/phone/actionPhone';
 
 const PhoneComponent = (props) => {
+	const phones = useSelector((state) => state.phones);
 	return (
 		<div className='container'>
 			<img src={phone} alt='phone' />
 			<p>
 				Disponibilite:
-				<span id='count'>{props.phones}</span>
+				<span id='count'>{phones}</span>
 			</p>
-			<button onClick={props.buyPhone}>Acheter</button>
+			<button onClick={() => null}>Acheter</button>
 		</div>
 	);
 };
 
-const mapStateToProps = (state) => {
-	return {
-		phones: state.phones,
-	};
-};
+// const mapStateToProps = (state) => {
+// 	return {
+// 		phones: state.phones,
+// 	};
+// };
 
-const mapDispatchToProps = (dispatch) => {
-	return {
-		buyPhone: () => dispatch(buyPhone()),
-		//we can also do : dispatch({ type: "BUY_PHONE"})
-	};
-};
+// const mapDispatchToProps = (dispatch) => {
+// 	return {
+// 		buyPhone: () => dispatch(buyPhone()),
+// 		//we can also do : dispatch({ type: "BUY_PHONE"})
+// 	};
+// };
 
-export default connect(mapStateToProps, mapDispatchToProps)(PhoneComponent);
+// export default connect(mapStateToProps, mapDispatchToProps)(PhoneComponent);
+
+export default PhoneComponent;
