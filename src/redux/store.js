d7d9f6846323c6275a732b1d65ff1c21f016799c
cedric karungu <ckarungu921@gmail.com>
2022-10-07 13:46:17 +0200

    feat: add redux-dev-tools

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,13 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import phoneReducer from './phone/reducerPhone';
 import commentsReducer from './comments/reducerComment';
 import tvReducer from './tv/reducerTv';
 import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+//for redux dev-tool
+//  first choice
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const rootReducers = combineReducers({
 	tv: tvReducer,
@@ -10,6 +15,10 @@ const rootReducers = combineReducers({
 	comments: commentsReducer,
 });
 
-const store = createStore(rootReducers, applyMiddleware(thunk));
+const store = createStore(
+	rootReducers,
+	// composeEnhancers(applyMiddleware(thunk)) //first choice
+	composeWithDevTools(applyMiddleware(thunk)) //second  choice
+);
 
 export default store;
