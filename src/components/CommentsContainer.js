3e6a047da874b1f57b96fdb716aaebe14f46ce4c
cedric karungu <ckarungu921@gmail.com>
2022-10-07 12:07:22 +0200

    feat:display data from redux-chunk middleware

--- src/components/CommentsContainer.js
+++ src/components/CommentsContainer.js
@@ -1,7 +1,39 @@
-import React from 'react';
-
-const CommentsContainer = () => {
-	return <div>hello</div>;
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { apiCall } from '../redux/comments/actionComment';
+const CommentsContainer = ({ apiData, apiComment }) => {
+	useEffect(() => {
+		apiComment();
+	}, [apiComment]);
+	const displayData = apiData.isLoading ? (
+		<p>Loading ...</p>
+	) : apiData.error ? (
+		<p>{apiData.error}</p>
+	) : (
+		<div>
+			{apiData.comments.slice(0, 10).map((comment) => (
+				<div key={comment.id} className='container-comment'>
+					<h2>{comment.name}</h2>
+					<h6>{comment.email}</h6>
+					<p>{comment.body}</p>
+					<hr />
+				</div>
+			))}
+		</div>
+	);
+	return <div>{displayData}</div>;
 };
 
-export default CommentsContainer;
+const mapStateToProps = (state) => {
+	return {
+		apiData: state.comments,
+	};
+};
+
+const mapDispatchToProps = (dispatch) => {
+	return {
+		apiComment: () => dispatch(apiCall()),
+	};
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(CommentsContainer);
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,6 +2,7 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import phoneReducer from './phone/reducerPhone';
 import commentsReducer from './comments/reducerComment';
 import tvReducer from './tv/reducerTv';
+import thunk from 'redux-thunk';
 
 const rootReducers = combineReducers({
 	tv: tvReducer,
@@ -9,6 +10,6 @@ const rootReducers = combineReducers({
 	comments: commentsReducer,
 });
 
-const store = createStore(rootReducers, applyMiddleware());
+const store = createStore(rootReducers, applyMiddleware(thunk));
 
 export default store;
