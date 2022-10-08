9ecef2a59171f639f506787ab174a9005bcddd47
cedric karungu <ckarungu921@gmail.com>
2022-10-08 16:20:44 +0200

    feat: create an addbook form

--- book/src/components/Navbar.jsx
+++ book/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Navbar = () => {
 	return (
-		<header>
+		<header style={{ position: 'absolute', right: 0, left: 0, top: 0 }}>
 			<div className='d-flex flex-column flex-md-row p-3 border-bottom bg-danger text-white'>
 				<h4 className='mr-md-auto'>
 					<a href='/' className='text-decoration-none text-white'>
--- book/src/containers/AddBooks.js
+++ book/src/containers/AddBooks.js
@@ -1,7 +1,85 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const AddBooks = () => {
-	return <div>AddBooks</div>;
+	const [state, setState] = useState({
+		title: '',
+		author: '',
+	});
+
+	const handlerChange = (e) => {
+		e.preventDefault();
+		setState((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log(state);
+	};
+	return (
+		<main role='main'>
+			<div className='jumbotron jumbotron-fluid bg-light p-5'>
+				<div className='container text-center mt-5'>
+					<div className='display-4'>Books</div>
+					<p>Ajouter un livre</p>
+
+					<form
+						action=''
+						className='form-inline d-flex justify-content-center flex-xs-column flex-wrap gap-2'
+					>
+						<div className='form-group'>
+							<input
+								type='text'
+								name='title'
+								id='title'
+								className='form-control'
+								placeholder='Title'
+								required
+								onChange={handlerChange}
+							/>
+						</div>
+						<div className='form-group'>
+							<input
+								type='text'
+								name='author'
+								id='author'
+								className='form-control'
+								placeholder='Autor'
+								required
+								onChange={handlerChange}
+							/>
+						</div>
+						<div className='form-group'>
+							<button
+								type='button'
+								name=''
+								id=''
+								className='btn btn-danger'
+								onClick={handleSubmit}
+							>
+								Add book
+							</button>
+						</div>
+					</form>
+				</div>
+			</div>
+			<div className='container mt-5' style={{ minHeight: 200 }}>
+				<div className='row'>
+					<div className='col-md-12'>
+						<ul className='list-group'>
+							<li className='list-group-item list-group-danger d-flex justify-content-between'>
+								Books saved
+							</li>
+						</ul>
+					</div>
+					<div className='d-flex justify-content-center'>
+						<button className='btn btn-danger mt-4 mb-5'>
+							Delete all books
+						</button>
+					</div>
+				</div>
+			</div>
+		</main>
+	);
 };
 
 export default AddBooks;
